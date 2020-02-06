93aef4de2fb532599e85a87f6f895e08cca761f1
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-02-06 16:09:09 +0800

    Add project files.

---
+++ TrialApplication/Models/Notes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrialApplication.Models
+{
+    class Notes
+    {
+
+        public string FileName { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
---
+++ TrialApplication/NoteEntryPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrialApplication.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrialApplication
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+        public async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Notes)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.FileName))
+            {
+                // Save
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                // Update
+                File.WriteAllText(note.FileName, note.Text);
+            }
+
+            await Navigation.PopAsync();
+        }
+        public void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
